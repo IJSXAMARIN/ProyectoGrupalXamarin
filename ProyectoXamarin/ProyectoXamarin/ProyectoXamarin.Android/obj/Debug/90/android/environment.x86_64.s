7eeee70b69dd3e38c8b262312c3c5b5d2cb8150a
JesusSaenz-Dev <61250238+JesusSaenz-Dev@users.noreply.github.com>
2020-06-01 18:39:12 +0200

    About us Diseño

--- ProyectoXamarin/ProyectoXamarin/ProyectoXamarin.Android/obj/Debug/90/android/environment.x86_64.s
+++ ProyectoXamarin/ProyectoXamarin/ProyectoXamarin.Android/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"979f106d-1eec-4772-960a-97792f8717ac"
+	.asciz	"4be40dad-586d-490a-8c08-f22a6b5ac8ff"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
